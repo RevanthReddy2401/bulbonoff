e7f0c24f91271e2ebde534aa5f527daab2fe4107
RevanthReddy2401 <163289624+RevanthReddy2401@users.noreply.github.com>
2024-03-18 22:45:14 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,10 @@
+function Bulbon()
+{ 
+    document.getElementById("img").src="bulbon-removebg-preview.png";
+
+}
+function Bulboff()
+{ 
+    document.getElementById("img").src="bulboff-removebg-preview.png";
+
+}
